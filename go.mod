45c2e5e6083879cd083ef9eae75fba52dad568fe
hxia <xiahuyun043@126.com>
2025-03-15 14:20:43 +0800

    add flag pacakge for iam project

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/TroyXia/component-base
+
+go 1.23.3
+
+require (
+	github.com/TroyXia/log v0.0.0-20250315060729-7f8500e0513e
+	github.com/spf13/pflag v1.0.6
+)
+
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
---
+++ pkg/cli/flag/flags.go
@@ -0,0 +1,25 @@
+package flag
+
+import (
+	"strings"
+
+	"github.com/TroyXia/log"
+	"github.com/spf13/pflag"
+)
+
+func WordSepNormalizeFunc(f *pflag.FlagSet, name string) pflag.NormalizedName {
+	if strings.Contains(name, "_") {
+		return pflag.NormalizedName(strings.ReplaceAll(name, "_", "-"))
+	}
+	return pflag.NormalizedName(name)
+}
+
+func WarnWordSepNormalizeFunc(f *pflag.FlagSet, name string) pflag.NormalizedName {
+	if strings.Contains(name, "_") {
+		nname := strings.ReplaceAll(name, "_", "-")
+		log.Warnf("%s is DEPRECATED and will be removed in a future version. Use %s instead.", name, nname)
+
+		return pflag.NormalizedName(nname)
+	}
+	return pflag.NormalizedName(name)
+}
